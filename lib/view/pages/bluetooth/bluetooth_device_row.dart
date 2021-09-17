ea33b1ddbc81ccf60d40d44e487006d2828b0efd
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-09-17 22:45:42 +0200

    Update bluez and setstate at dialogclose

--- lib/view/pages/bluetooth/bluetooth_device_row.dart
+++ lib/view/pages/bluetooth/bluetooth_device_row.dart
@@ -113,6 +113,6 @@ class _BluetoothDeviceRowState extends State<BluetoothDeviceRow> {
                   ),
                 ),
               );
-            }));
+            })).then((value) => setState(() {}));
   }
 }
